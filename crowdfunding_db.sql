e98c1e6c340a314ad29d1aad59d55d3950b58f63
Naivebody <2896968767@qq.com>
2024-09-30 04:00:20 +0800

    Create crowdfunding_db.sql

---
+++ crowdfunding_db.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: crowdfunding_db
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `CATEGORY_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(20) NOT NULL,
+  PRIMARY KEY (`CATEGORY_ID`),
+  UNIQUE KEY `category_NAME_uindex` (`NAME`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` (`CATEGORY_ID`, `NAME`) VALUES (1,'Construction'),(3,'Entertainment'),(2,'Internet');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fundraiser`
+--
+
+DROP TABLE IF EXISTS `fundraiser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fundraiser` (
+  `FUNDRAISER_ID` int NOT NULL AUTO_INCREMENT,
+  `ORGANIZER` varchar(30) NOT NULL,
+  `CAPTION` varchar(20) NOT NULL,
+  `TARGET_FUNDING` bigint NOT NULL,
+  `CURRENT_FUNDING` bigint DEFAULT NULL,
+  `CITY` varchar(20) DEFAULT NULL,
+  `ACTIVE` tinyint(1) NOT NULL,
+  `CATEGORY_ID` int DEFAULT NULL,
+  PRIMARY KEY (`FUNDRAISER_ID`),
+  KEY `fundraiser_category_fk` (`CATEGORY_ID`),
+  CONSTRAINT `fundraiser_category_fk` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fundraiser`
+--
+
+LOCK TABLES `fundraiser` WRITE;
+/*!40000 ALTER TABLE `fundraiser` DISABLE KEYS */;
+INSERT INTO `fundraiser` (`FUNDRAISER_ID`, `ORGANIZER`, `CAPTION`, `TARGET_FUNDING`, `CURRENT_FUNDING`, `CITY`, `ACTIVE`, `CATEGORY_ID`) VALUES (6,'Alibaba','Server need',400000,150000,'Sydney',1,2),(8,'Jackie Chen','New film',200000,100000,'Hong Kong',1,3),(15,'Tencent','Construction',600000,100000,'Beijing',1,1),(32,'Valve','Deadlock dev',150000,50000,'LA',0,3),(34,'Jay Chou','New songs',50000,20000,'Shanghai',1,3);
+/*!40000 ALTER TABLE `fundraiser` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-30  3:50:47
