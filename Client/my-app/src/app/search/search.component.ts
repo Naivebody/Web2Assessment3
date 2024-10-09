4448b767a83d9295a0e595e7113923baaef9d820
Mr.Dream <146167041+MrDreamA@users.noreply.github.com>
2024-10-10 00:52:19 +0800

    Part of search

--- Client/my-app/src/app/search/search.component.ts
+++ Client/my-app/src/app/search/search.component.ts
@@ -1,4 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+//Services
+@Injectable({
+  providedIn: 'root'
+})
+export class FundraiserService {
+  private apiUrl = 'http://localhost:3060/api/fundraiser';
+
+  constructor(private http: HttpClient) {}
+
+  searchFundraisers(criteria: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/search`, criteria);
+  }
+
+  getFundraiser(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  addFundraiser(fundraiser: any): Observable<any> {
+    return this.http.post(this.apiUrl, fundraiser);
+  }
+
+  addDonation(donation: any): Observable<any> {
+    return this.http.post('http://localhost:3060/api/donation', donation);
+  }
+}
 
 @Component({
   selector: 'app-search',
