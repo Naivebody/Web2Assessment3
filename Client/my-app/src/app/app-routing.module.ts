0979dc252b2e75aa26bdfdb949aa9d996109feb5
yekun <2896968767@qq.com>
2024-10-09 09:49:57 +0800

    Router init

--- Client/my-app/src/app/app-routing.module.ts
+++ Client/my-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {SearchComponent} from './search/search.component';
+import {FundraiserDetailComponent} from './fundraiser-detail/fundraiser-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',component: HomeComponent},
+  {path: 'search',component: SearchComponent},
+  {path: 'fundraiser',component: FundraiserDetailComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
