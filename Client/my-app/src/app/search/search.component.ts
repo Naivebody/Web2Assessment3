d271e5b032b501b8a73ec8275123281fffb8f8d1
Mr.Dream <146167041+MrDreamA@users.noreply.github.com>
2024-10-10 21:36:37 +0800

    Part_search_2

--- Client/my-app/src/app/search/search.component.ts
+++ Client/my-app/src/app/search/search.component.ts
@@ -28,7 +28,64 @@ export class FundraiserService {
     return this.http.post('http://localhost:3060/api/donation', donation);
   }
 }
+//Search
+@Component({
+  selector: 'app-search',
+  templateUrl: '.arch.component.html',
+  styleUrls: ['.arch.component.css']
+})
+export class SearchComponents {
+  criteria = {
+    organizer: '',
+    city: '',
+    category: ''
+  };
+  fundraisers: any[] = [];
+  message: string = '';
 
+  constructor(private fundraiserService: FundraiserService) {}
+
+  searchFundraisers(): void {
+    this.fundraiserService.searchFundraisers(this.criteria).subscribe(
+      data => {
+        this.fundraisers = data;
+      },
+      error => {
+        this.message = 'Fail';
+        console.error('Error searching fundraisers', error);
+      }
+    );
+  }
+}
+
+//Fundraiser
+@Component({
+  selector: 'app-fundraiser',
+  templateUrl: './fundraiser.component.html',
+  styleUrls: ['./fundraiser.component.css']
+})
+export class FundraiserComponent implements OnInit {
+  fundraiser: any;
+  errorMessage: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private fundraiserService: FundraiserService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.fundraiserService.getFundraiser(id).subscribe(
+      data => {
+        this.fundraiser = data;
+      },
+      error => {
+        this.errorMessage = 'Fail';
+        console.error('Error fetching data', error);
+      }
+    );
+  }
+}
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
