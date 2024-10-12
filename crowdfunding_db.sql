975a20bc79a79c0eb77f16e8c5fbb8e0a9c3956d
yekun <2896968767@qq.com>
2024-10-12 16:09:58 +0800

    Update crowdfunding_db.sql
    Database update for donation

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -27,7 +27,7 @@ CREATE TABLE `category` (
   `NAME` varchar(20) NOT NULL,
   PRIMARY KEY (`CATEGORY_ID`),
   UNIQUE KEY `category_NAME_uindex` (`NAME`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,8 +36,37 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` (`CATEGORY_ID`, `NAME`) VALUES (1,'Construction'),(3,'Entertainment'),(2,'Internet');
+INSERT INTO `category` (`CATEGORY_ID`, `NAME`) VALUES (5,'Art'),(1,'Construction'),(3,'Entertainment'),(2,'Internet'),(4,'Medical');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `donation`
+--
+
+DROP TABLE IF EXISTS `donation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donation` (
+  `DONATION_ID` int NOT NULL AUTO_INCREMENT,
+  `DATE` date NOT NULL,
+  `AMOUNT` int NOT NULL,
+  `GIVER` varchar(20) NOT NULL,
+  `FUNDRAISER_ID` int NOT NULL,
+  PRIMARY KEY (`DONATION_ID`),
+  KEY `donation_fundraiser_FUNDRAISER_ID_fk` (`FUNDRAISER_ID`),
+  CONSTRAINT `donation_fundraiser_FUNDRAISER_ID_fk` FOREIGN KEY (`FUNDRAISER_ID`) REFERENCES `fundraiser` (`FUNDRAISER_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `donation`
+--
+
+LOCK TABLES `donation` WRITE;
+/*!40000 ALTER TABLE `donation` DISABLE KEYS */;
+INSERT INTO `donation` (`DONATION_ID`, `DATE`, `AMOUNT`, `GIVER`, `FUNDRAISER_ID`) VALUES (1,'2024-09-01',50,'Zihan Wei',3),(2,'2024-09-02',100,'Yunlin Li',1),(3,'2024-10-01',200000,'Tencent',3),(4,'2024-10-03',40000,'Alibaba',8),(5,'2024-10-05',50,'Kun Ye',9);
+/*!40000 ALTER TABLE `donation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -59,7 +88,7 @@ CREATE TABLE `fundraiser` (
   PRIMARY KEY (`FUNDRAISER_ID`),
   KEY `fundraiser_category_fk` (`CATEGORY_ID`),
   CONSTRAINT `fundraiser_category_fk` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +97,7 @@ CREATE TABLE `fundraiser` (
 
 LOCK TABLES `fundraiser` WRITE;
 /*!40000 ALTER TABLE `fundraiser` DISABLE KEYS */;
-INSERT INTO `fundraiser` (`FUNDRAISER_ID`, `ORGANIZER`, `CAPTION`, `TARGET_FUNDING`, `CURRENT_FUNDING`, `CITY`, `ACTIVE`, `CATEGORY_ID`) VALUES (1,'Alibaba','Server need',400000,150000,'Sydney',1,2),(2,'Jackie Chen','New film',200000,100000,'Hong Kong',1,3),(3,'Tencent','Construction',600000,100000,'Beijing',1,1),(4,'Valve','Deadlock dev',150000,50000,'LA',0,3),(5,'Jay Chou','New songs',50000,20000,'Shanghai',1,3);
+INSERT INTO `fundraiser` (`FUNDRAISER_ID`, `ORGANIZER`, `CAPTION`, `TARGET_FUNDING`, `CURRENT_FUNDING`, `CITY`, `ACTIVE`, `CATEGORY_ID`) VALUES (1,'Alibaba','Server need',400000,150000,'Sydney',1,2),(2,'Jackie Chen','New film',200000,100000,'Hong Kong',1,3),(3,'Tencent','Construction',600000,100000,'Beijing',1,1),(5,'Jay Chou','New songs',50000,20000,'Shanghai',1,3),(6,'Jay Chou','New schools',600000,300000,'Chongqing',1,5),(7,'Yunlin Li','Esports',200000,2000,'Liuzhou',0,3),(8,'Zihan Wei','Medical equipments',500000,100000,'Liuzhou',1,4),(9,'Yuezhong','Books for pupils',40000,20000,'Liuzhou',1,5),(10,'Reza','New roads',650000,300000,'Brisbane',0,1);
 /*!40000 ALTER TABLE `fundraiser` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -81,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-30  7:58:04
+-- Dump completed on 2024-10-12 16:08:52
