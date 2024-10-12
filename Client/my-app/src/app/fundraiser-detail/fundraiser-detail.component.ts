ea1a5fc095cdc9f6c078ec2ce7fb94895b3a1b90
yekun <2896968767@qq.com>
2024-10-12 21:34:39 +0800

    Display on fundraiserdetail

--- Client/my-app/src/app/fundraiser-detail/fundraiser-detail.component.ts
+++ Client/my-app/src/app/fundraiser-detail/fundraiser-detail.component.ts
@@ -10,6 +10,8 @@ import {FundraiserResponse} from '../class/FundraiserResponse';
   styleUrl: './fundraiser-detail.component.css'
 })
 export class FundraiserDetailComponent implements OnInit {
+  imgNum : number[] =[1,2,3,4,5]
+  isActive: boolean = false;
   dataID = 0;
   fundraiser : Fundraiser[]=[]
  constructor(private dataService: DataService,private router: ActivatedRoute) { }
@@ -27,6 +29,9 @@ export class FundraiserDetailComponent implements OnInit {
    this.dataService.getFundraiserByID(this.dataID).subscribe(
      (response: Fundraiser[]) =>{
        this.fundraiser = response;
+       if(this.fundraiser[0].active ===1){
+         this.isActive = true;
+       }
        console.log(this.fundraiser);
      }
    )
